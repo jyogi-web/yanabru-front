183cdfa6f044a47e5dc5a540bf7fd19df5fdf971
RiTa-23 <rita.tifrt@gmail.com>
2024-09-14 01:48:47 +0900

    [add][modify] お手本動画の再生

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 ## 仮想環境(venv)
-https://qiita.com/shun_sakamoto/items/7944d0ac4d30edf91fde
+### 仮想環境作成
+以下のコマンドを実行して仮想環境を作成する
+```bash
+python -m venv [仮想環境名]
+```
+### 仮想環境のアクティベート
+#### Linux,Macの場合
+```bash
+ .\[仮想環境名]/bin/activate
+```
+#### Windowsの場合
+```bash
+.\[仮想環境名]\Scripts\activate
+```
+
+### 依存関係書き出し
+```bash
+pip freeze > requirements.txt
+```
+
+## 実行
+```bash
+python run.py
+```
+
+参考：(https://qiita.com/shun_sakamoto/items/7944d0ac4d30edf91fde)
--- app/app.py
+++ app/app.py
@@ -12,66 +12,31 @@ def index():
 # MediaPipeの初期化
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
-mp_drawing = mp.solutions.drawing_utils
 
-# カメラ映像を取得
+# お手本動画の読み込み
+video_path = '/static/video/エイサー_島人ぬ宝.mp4'
+cap = cv2.VideoCapture(video_path)
+
 def generate_frames():
-    cap = cv2.VideoCapture(0)  # カメラの映像を取得（0はデフォルトカメラ）
-
     while True:
         success, frame = cap.read()
         if not success:
             break
 
-        # 画像をRGBに変換
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # 骨格検出を行う
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = pose.process(frame_rgb)
 
-        # MediaPipeで骨格検出
-        result = pose.process(rgb_frame)
+        # 骨格の描画
+        if results.pose_landmarks:
+            mp.solutions.drawing_utils.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-        # 骨格のランドマークを描画
-        if result.pose_landmarks:
-            mp_drawing.draw_landmarks(
-                frame, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-
-        # JPEG形式でエンコード
+        # フレームをJPEG形式にエンコードして送信
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
 
-        # フレームを返す
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-#入力：姿勢推定から得られたランドマークのデータ
-#出力：12の点(右肩)からの相対座標(numpy配列)
-def landmark2np(pose_landmarks):
-    detected_point = 12
-    li = []
-    for j in (pose_landmarks.landmark):
-            li.append([j.x, j.y, j.z])
-    for k in li:
-        if k[0] == 0 and k[1] == 0  and k[2]==0:
-            print("No detected")
-            li[k] = li[detected_point]
-
-    return np.array(li) - li[detected_point]
-
-#入力：比較する2つの座標ランドマーク群
-#出力：各ベクトルごとに比較したコサイン平均類似度
-def manual_cos(A, B):
-    dot = np.sum(A*B, axis=-1)
-    A_norm = np.linalg.norm(A, axis=-1)
-    B_norm = np.linalg.norm(B, axis=-1)
-    cos = dot / (A_norm*B_norm+1e-10)
-
-    # 検出できない場合の処理
-    for i in cos:
-        count = 0
-        if i == 0:
-            print("cos deleted")
-            np.delete(cos,count)
-        count = count +1
-    return cos.mean()
 
 # ルートエンドポイント
 @app.route('/video')
