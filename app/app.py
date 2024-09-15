0cfa12c661f0d86a37a672a0981fa6d66133f676
RiTa-23 <rita.tifrt@gmail.com>
2024-09-15 16:38:11 +0900

    [add] #19 曲選択機能

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request,url_for
 import json
 
 app = Flask(__name__)
 
 # ランドマークデータの保存先
 landmarks_file = 'app/static/landmarks/suirenka-mini_landmarks.json'
+# ランドマークファイル更新
+def update_landmarks(file):
+    global landmarks_file
+    landmarks_file=file
 
 # ランドマークデータを提供するエンドポイント
 @app.route('/get-pose-landmarks', methods=['GET'])
@@ -27,9 +31,24 @@ def get_pose_landmarks():
 def index():
     return render_template('index.html')
 
-@app.route('/game')
-def play():
-    return render_template('game.html')
+
+@app.route('/eisa')
+def eisa():
+    link='/eisa'
+    update_landmarks('app/static/landmarks/sample_landmarks(1m).json')
+    #landmarks_file='app/static/landmarks/sample_landmarks(1m).json'
+    # 動画ファイルのパスを動的に生成
+    video_file = url_for('static', filename='video/sample_1m.mp4')
+    return render_template('game.html',video_file=video_file,link=link)
+
+@app.route('/syounaxn')
+def syounaxn():
+    link='/syounaxn'
+    update_landmarks('app/static/landmarks/suirenka-mini_landmarks.json')
+    #landmarks_file = 'app/static/landmarks/suirenka-mini_landmarks.json'
+    # 動画ファイルのパスを動的に生成
+    video_file = url_for('static', filename='video/JustDance_Suirenka_mini.mp4')
+    return render_template('game.html',video_file=video_file,link=link)
 
 @app.route('/landtest')
 def landtest():
