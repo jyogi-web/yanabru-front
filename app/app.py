6a9fa1dc1edc70b39b6876f281cb4c16deefff26
Yanai1005 <tatsuya871_1005@icloud.com>
2024-11-08 13:00:10 +0900

    modify:動画の削除

--- app/app.py
+++ app/app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, jsonify, request,url_for
+from flask import Flask, render_template, jsonify, request,url_for, redirect
 import json
 import subprocess
-from .Joycon import joycon
+# from .Joycon import joycon
 import subprocess
 import threading
 import time
 from flask_cors import CORS 
 import os
 import cv2
+import glob
 import mediapipe as mp
 from werkzeug.utils import secure_filename
 
@@ -30,7 +31,7 @@ LANDMARKS_FOLDER = os.path.join(BASE_DIR, 'static', 'landmarks')
 os.makedirs(LANDMARKS_FOLDER, exist_ok=True)
 
 # アップロードフォルダの設定
-UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
+UPLOAD_FOLDER = os.path.join(BASE_DIR,'static', 'uploads')
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # ランドマークデータの保存先
@@ -132,7 +133,8 @@ def upload_video():
         return jsonify({'status': 'error', 'message': 'Failed to save video file'}), 500
     
     # ランドマークの保存先パスを設定
-    save_filename = f"{os.path.splitext(filename)[0]}_landmarks.json"
+    # save_filename = f"{os.path.splitext(filename)[0]}_landmarks.json"
+    save_filename = f"landmarks.json"
     save_path = os.path.join(LANDMARKS_FOLDER, save_filename)
     print(f"ランドマーク保存先のパス: {save_path}")
     
@@ -154,68 +156,6 @@ def index():
     games = [os.path.splitext(f)[0] for f in video_files]
     return render_template('index.html', games=games)
 
-
-@app.route('/eisa')
-def eisa():
-    link='/eisa'
-    update_landmarks('app/static/landmarks/sample_landmarks(1m).json')
-    #landmarks_file='app/static/landmarks/sample_landmarks(1m).json'
-    # 動画ファイルのパスを動的に生成
-    video_file = url_for('static', filename='video/sample_1m.mp4')
-    return render_template('game.html',video_file=video_file,link=link)
-
-@app.route('/suirenka')
-def syounaxn():
-    link='/suirenka'
-    update_landmarks('app/static/landmarks/suirenka-mini_landmarks.json')
-    #landmarks_file = 'app/static/landmarks/suirenka-mini_landmarks.json'
-    # 動画ファイルのパスを動的に生成
-    video_file = url_for('static', filename='video/JustDance_Suirenka_mini.mp4')
-    return render_template('game.html',video_file=video_file,link=link)
-
-@app.route('/odoloop')
-def odoloop():
-    link='/odoloop'
-    update_landmarks('app/static/landmarks/odottezunda_landmarks.json')
-    #landmarks_file = 'app/static/landmarks/suirenka-mini_landmarks.json'
-    # 動画ファイルのパスを動的に生成
-    video_file = url_for('static', filename='video/踊ってない夜を知らないずんだもん反転_mini.mp4')
-    return render_template('game.html',video_file=video_file,link=link)
-
-@app.route('/marumori')
-def marumori():
-    link='/marumori'
-    update_landmarks('app/static/landmarks/marumori_landmarks.json')
-    #landmarks_file = 'app/static/landmarks/suirenka-mini_landmarks.json'
-    # 動画ファイルのパスを動的に生成
-    video_file = url_for('static', filename='video/マル・マル・モリ・モリ_反転.mp4')
-    return render_template('game.html',video_file=video_file,link=link)
-
-@app.route('/dynamic')
-def dynamic():
-    link='/dynamic'
-    update_landmarks('app/static/landmarks/dynamic_landmarks.json')
-    #landmarks_file = 'app/static/landmarks/suirenka-mini_landmarks.json'
-    # 動画ファイルのパスを動的に生成
-    video_file = url_for('static', filename='video/ダイナミック琉球反転_mini.mp4')
-    return render_template('game.html',video_file=video_file,link=link)
-
-@app.route('/koi')
-def koi():
-    link='/koi'
-    update_landmarks('app/static/landmarks/koidansu_landmarks.json')
-    # 動画ファイルのパスを動的に生成
-    video_file = url_for('static', filename='video/恋ダンス振り付け反転.mp4')
-    return render_template('game.html',video_file=video_file,link=link)
-
-@app.route('/harehare')
-def harehare():
-    link='/harehare'
-    update_landmarks('app/static/landmarks/harehare_landmarks.json')
-    # 動画ファイルのパスを動的に生成
-    video_file = url_for('static', filename='video/ハレ晴レユカイ反転.mp4')
-    return render_template('game.html',video_file=video_file,link=link)
-  
 @app.route('/game/<game_name>')
 def game_page(game_name):
     """
@@ -223,7 +163,7 @@ def game_page(game_name):
     ランドマークファイルと動画ファイルが存在するかを確認し、
     存在する場合はgame.htmlテンプレートにデータを渡します。
     """
-    landmark_file = os.path.join(LANDMARKS_FOLDER, f'{game_name}_landmarks.json')
+    landmark_file = os.path.join(LANDMARKS_FOLDER, f'landmarks.json')
     video_file = f'uploads/{game_name}.mp4'
 
     # 動画ファイルのフルパス
@@ -243,37 +183,14 @@ def game_page(game_name):
 
     return render_template('game.html', video_file=video_url, game_name=game_name)
 
-@app.route('/eigadancer')
-def eigadancer():
-    return render_template('eigadancer.html')
 
-@app.route('/landtest')
-def landtest():
-    return render_template('landtest.html')
-
-# Joycon.pyを実行するためのAPI
-@app.route('/run-joycon', methods=['POST'])
-def run_joycon():
-    print("runrunrun")
-    try:
-        # Joycon.pyをバックグラウンドで実行
-        process = subprocess.Popen(['python', '../Joycon.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        if stderr:
-            return jsonify({'status': 'Joycon.py failed', 'error': stderr.decode()}), 500
-        return jsonify({'status': 'Joycon.py started'}), 200
-    except Exception as e:
-        return jsonify({'status': 'Error', 'error': str(e)}), 500
-
-
-# Joy-Conの処理をバックグラウンドで実行するエンドポイント
-@app.route('/start-joycon', methods=['POST'])
-def start_joycon():
-    print("start_joycon!!")
-    start_time = time.time()  # スタートボタンが押された時の時間を記録
-    # Joyconの処理をバックグラウンドで実行
-    threading.Thread(target=joycon, args=(global_score, score_lock, start_time)).start()
-    return jsonify({'status': 'Joy-Con processing started'}), 200
+# 動画削除
+@app.route('/delete_video')
+def delete_video():
+    video_files = [f for f in os.listdir(app.config['UPLOAD_FOLDER']) if f.endswith('.mp4')]
+    for video_file in video_files:
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], video_file))
+    return redirect(url_for('index'))
 
 @app.route('/get-score', methods=['GET'])
 def get_score():
